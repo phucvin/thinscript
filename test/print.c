61232ca2ca78241cfcb27ab41a8ec3a295290f85
phucvin <phucvin@gmail.com>
2024-10-05 05:40:28 +0000

    print correctly

--- test/print.c
+++ test/print.c
@@ -1,6 +1,10 @@
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "t01.h"
 
 void print(const uint16_t *text) {
-  printf("TODO: print uint16_t* string\n");
+  uint8_t *ctext = utf16_to_cstring(text);
+  printf("C injected | %s\n", (const char*)ctext);
+  free(ctext);
 }
